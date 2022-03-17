f54dfbc3278fec70fc5db70eb1a77819dba0cb4a
aleksandraurchenko <alex_bloom@mail.ru>
2022-03-17 21:56:50 +0300

    fix

--- src/Main.java
+++ src/Main.java
@@ -6,26 +6,13 @@ public class Main {
         int bonus = entrance / 100;
         int totalAmount = clientAccount + entrance + bonus;
 
-        if (entrance<1000) {
+        if (entrance < 1000) {
             System.out.println("сумма счета" + (clientAccount + entrance) + "рублей");
-        }
-        else {
+        } else {
             System.out.println("сумма счета" + (totalAmount) + "рублей");
         }
 
 
-
-
-
-
-        // Объявляете переменные для входных данных и
-        // параметров программы: начального счёта,
-        // суммы пополнения и тп
-
-        // Условным оператором проверяете превысила ли
-        // сумма пополнения порог и для этих двух разных
-        // сценариев рассчитываете сумму бонуса и выводите
-        // на экран.
     }
 }
 
